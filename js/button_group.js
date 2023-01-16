0d2e78b50f7125c0dfb480127a934a30740dd42c
Steven Lyles <steven.lyles@gmail.com>
2023-01-15 18:27:39 -0800

    Added finished button group widget to example

--- js/button_group.js
+++ js/button_group.js
@@ -9,29 +9,29 @@
 'use strict';
 
 let global_config = {};
-let global_id = 0;
 let button_group_json = $.getJSON({'url': "../config/color_map.json", 'async': false});
 let button_group_color = JSON.parse(button_group_json.responseText);
 
 //=======================================================================================
-// Creates a widget for displaying a wrapping group array of buttons with actions
+// Creates a widget for displaying a wrapping group array of buttons with callback action
 class ButtonGroup {
     constructor(container_id, config, callback) {
-        this.config = config;
-        global_config = config;
-        this.id = container_id;
         this.widget_id = this.gen_unique_id_from(container_id);
-        global_id = this.widget_id;
-        global_config["callback"] = callback;
+        this.config = config;
+        global_config[this.widget_id] = this.config;
+        global_config[this.widget_id]["callback"] = callback;
+        this.id = container_id;
 
         this.gen_widget();
         this.gen_css();
     }
 
+    //===================================================================================
+    // Creates a unique hash id for this widget instance based on the container id
     gen_unique_id_from(str) {
-        var hash = Math.random() * (10 - 0) + 0;
-        var i = 0;
-        var char = "";
+        let hash = Math.random() * (10 - 0) + 0;
+        let i = 0;
+        let char = "";
 
         if (str.length !== 0) {
             for (i = 0; i < str.length; i++) {
@@ -44,27 +44,35 @@ class ButtonGroup {
     }
 
     //===================================================================================
+    // Generate CSS for this instance based on config attributes
     gen_css() {
+        $(`#${this.id}`).css("display", "flex");
+        $(`#${this.id}`).css("flex-direction", "row");
+        $(`#${this.id}`).css("align-items", "center");
+        $(`#${this.id}`).css("justify-content", "right");
         $(`#${this.id}`).css("flex-wrap", "wrap");
+
+        $(".button-group").css("margin-left", "10px");
+        $(".button-group").css("display", "flex");
+        $(".button-group").css("flex-direction", "row");
+        $(".button-group").css("align-items", "center");
+        $(".button-group").css("justify-content", "right");
+        $(".button-group").css("padding-right", "20px");
+
         $(`.button-group-${this.widget_id}`).css("font", this.config.font);
         $(`.button-group-${this.widget_id}`).css("font-size", this.config.font_size);
         $(`.button-group-${this.widget_id}`).css("width", this.config.width);
         $(`.button-group-${this.widget_id}`).css("height", this.config.height );
-        $(`.button-group-${this.widget_id}`).css("margin-left", "10px");
         $(`.button-group-${this.widget_id}`).css("border-radius", this.config.border_radius);
-        $(`.button-group-${this.widget_id}`).css("display", "flex");
-        $(`.button-group-${this.widget_id}`).css("flex-direction", "row");
-        $(`.button-group-${this.widget_id}`).css("align-items", "center");
-        $(`.button-group-${this.widget_id}`).css("justify-content", "right");
         $(`.button-group-${this.widget_id}`).css("color", this.config.font_color);
-        $(`.button-group-${this.widget_id}`).css("padding-right", "20px");
 
         let bg_color = button_group_color[this.config.background_color].hex;
         let active_color = button_group_color[this.config.active_color].hex;
         let inactive_color = button_group_color[this.config.inactive_color].hex;
+        let global_id = this.widget_id;
 
         $.each(this.config.buttons, function(index, button) {
-            if (index == global_config.selected_index) {
+            if (index == global_config[global_id].selected_index) {
                 $(`#button-group-${index}-${global_id}`).css("background-color", `#${active_color}`);
                 $(`#button-group-${index}-${global_id}`).css("font-weight", "bold");
             } else {
@@ -80,16 +88,19 @@ class ButtonGroup {
     }
 
     //===================================================================================
+    // Generate the HTML in the container element
     gen_widget() {
         let default_index = this.config.default_index;
         let widget_id = this.widget_id
         let id = this.id;
         let config = this.config;
         $.each(this.config.buttons, function(index, value) {
-            $(`#${id}`).append(`  <div id='button-group-${index}-${widget_id}' class='button-group-${widget_id}'>${value.txt}</div>\n`);
+            $(`#${id}`).append(`  <div id='button-group-${index}-${widget_id}' class='button-group-${widget_id} button-group'>${value.txt}</div>\n`);
         });
     }
 
+    //===================================================================================
+    // Activates the button at index if not already enabled
     activate_button(index) {
         if (!this.config.buttons[index].active) {
             let button_id = `#button-group-${index}-${this.widget_id}`;
@@ -101,6 +112,8 @@ class ButtonGroup {
         }
     }
 
+    //===================================================================================
+    // Disables button at index if not already disabled
     deactivate_button(index) {
         if (this.config.buttons[index].active) {
             let button_id = `#button-group-${index}-${this.widget_id}`;
@@ -115,6 +128,12 @@ class ButtonGroup {
         }
     }
 
+    //===================================================================================
+    // Returns the index of the currently selected button (there can be only one)
+    get_selected_button_index() {
+        return this.config.selected_index;
+    }
+
 } /* class Now */
 
 //=======================================================================================
@@ -123,41 +142,45 @@ $(document).ready(function() {
 
     //--------------------------
     // Handle hover over buttons
-    $(`.button-group-${global_id}`).hover( function() {
+    $(".button-group").hover( function() {
+        let id = `${this.id}`.split("-")[3]
         $(`#${this.id}`).css("cursor", "pointer");
-        $(`#${this.id}`).css("background-color", `#${button_group_color[global_config.hover_color].hex}`);
-        $(`#${this.id}`).css("box-shadow", `0px 0px 0px 3px #${color[global_config.hover_outline].hex} inset`);
+        $(`#${this.id}`).css("background-color", `#${button_group_color[global_config[id].hover_color].hex}`);
+        $(`#${this.id}`).css("box-shadow", `0px 0px 0px 3px #${button_group_color[global_config[id].hover_outline].hex} inset`);
     }, function() {
+        let id = `${this.id}`.split("-")[3]
         $(`#${this.id}`).css("cursor", "default");
         let index = parseInt(this.id.split("-")[2]);
-        if (global_config.selected_index == index) {
-            $(`#${this.id}`).css("background-color", `#${button_group_color[global_config.active_color].hex}`);
-            $(`#${this.id}`).css("box-shadow", `0px 0px 0px 3px #${color[global_config.active_color].hex} inset`);
-        } else if (!global_config.buttons[index].active) {
-            $(`#${this.id}`).css("background-color", `#${button_group_color[global_config.inactive_color].hex}`);
-            $(`#${this.id}`).css("box-shadow", `0px 0px 0px 3px #${color[global_config.inactive_color].hex} inset`);
+        if (global_config[id].selected_index == index) {
+            $(`#${this.id}`).css("background-color", `#${button_group_color[global_config[id].active_color].hex}`);
+            $(`#${this.id}`).css("box-shadow", `0px 0px 0px 3px #${button_group_color[global_config[id].active_color].hex} inset`);
+        } else if (!global_config[id].buttons[index].active) {
+            $(`#${this.id}`).css("background-color", `#${button_group_color[global_config[id].inactive_color].hex}`);
+            $(`#${this.id}`).css("box-shadow", `0px 0px 0px 3px #${button_group_color[global_config[id].inactive_color].hex} inset`);
         } else {
-            $(`#${this.id}`).css("background-color", `#${button_group_color[global_config.background_color].hex}`);
-            $(`#${this.id}`).css("box-shadow", `0px 0px 0px 3px #${color[global_config.background_color].hex} inset`);
+            $(`#${this.id}`).css("background-color", `#${button_group_color[global_config[id].background_color].hex}`);
+            $(`#${this.id}`).css("box-shadow", `0px 0px 0px 3px #${button_group_color[global_config[id].background_color].hex} inset`);
         }
     });
 
     //------------------------------------
     // Handle callback when button clicked
-    $(`.button-group-${global_id}`).click( function() {
+    $(".button-group").click( function() {
         let index = parseInt(`${this.id}`.split("-")[2]);
-        if (global_config.buttons[index].active) {
-            let old_selected_button_id = `#button-group-${global_config.selected_index}-${global_id}`
-            $(old_selected_button_id).css("background-color", `#${button_group_color[global_config.background_color].hex}`);
-            $(old_selected_button_id).css("box-shadow", `0px 0px 0px 3px #${color[global_config.background_color].hex} inset`);
+        let id = `${this.id}`.split("-")[3]
+        if (global_config[id].buttons[index].active) {
+            let old_selected_button_id = `#button-group-${global_config[id].selected_index}-${id}`
+            $(old_selected_button_id).css("background-color", `#${button_group_color[global_config[id].background_color].hex}`);
+            $(old_selected_button_id).css("box-shadow", `0px 0px 0px 3px #${button_group_color[global_config[id].background_color].hex} inset`);
             $(old_selected_button_id).css("font-weight", "normal");
-            global_config.selected_index = index;
-            $(`#${this.id}`).css("background-color", `#${button_group_color[global_config.active_color].hex}`);
-            $(`#${this.id}`).css("box-shadow", `0px 0px 0px 3px #${color[global_config.active_color].hex} inset`);
+            global_config[id].selected_index = index;
+            $(`#${this.id}`).css("background-color", `#${button_group_color[global_config[id].active_color].hex}`);
+            $(`#${this.id}`).css("box-shadow", `0px 0px 0px 3px #${button_group_color[global_config[id].active_color].hex} inset`);
             $(`#${this.id}`).css("font-weight", "bold");
-            global_config.callback.apply(this, [index]);
+
+            global_config[id].callback.apply(this, [index]);
         } else {
-            console.log(`${global_config.buttons[index].txt} is not active.`);
+            console.log(`${global_config[id].buttons[index].txt} is not active.`);
         }
 
     });
